c4144bb5f09104524573e558c9bc7f115499a73a
Gjoifrjenfikdl1 <c22ab@dalton.org>
2017-01-12 10:50:45 -0500

    Update ArrayPlatformer.js

--- ArrayPlatformer.js
+++ ArrayPlatformer.js
@@ -6,21 +6,9 @@
 var canvas = document.getElementById("canvas"),
 ctx = canvas.getContext("2d"),
 //create width and height of canvas
-width = 1350/4,
-height = 580/4,
+width = 640,
+height = 640,
 //give starting player properties
-player = {
-  x : width/2,
-  y : height - 25,
-  width : 25,
-  height : 25,
-  speed: 20,
-  velX: 0,
-  velY: 0,
-  jumping : false,
-  jumpHeight: 0.3,
-  grounded: false
-},
 keys = [],
 friction = 0.8,
 gravity = 0.3;
@@ -29,150 +17,203 @@ canvas.width = width;
 canvas.height = height;
 
 var data=[
-    [1,0,0,0,1,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,1,0,0,1,0,0,0,0,0,1],
-    [1,0,0,0,1,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,1,1,1,1,1,1,1,1,1,1,1,1]
+    [1,2,0,0,1,0,0,0,0,0,0,0,0,1,0,1],
+    [1,0,0,0,1,0,0,1,0,0,0,0,0,1,0,1],
+    [1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+    [1,0,0,0,0,1,1,1,1,0,0,1,1,1,1,1],
+    [1,0,0,0,0,1,1,1,0,0,1,1,1,1,1,1],
+    [1,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1],
+    [1,0,0,0,0,1,0,0,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
     ];
 
-//holds boxes
-var boxes = [];
+// works for rectangles
+var collide=function(obj1,obj2){
+    if( obj1.x<obj2.x+obj2.w&&obj1.x+obj1.w>obj2.x&&
+        obj1.y<obj2.y+obj2.h&&obj1.y+obj1.h>obj2.y)
+    {
+        return true;   
+    }    
+};
+// Player Object
+var Player=function(x,y,w,h,keyInputs,color){
+    this.x=x;
+    this.y=y;
+    this.w=w;
+    this.h=h;
+    
+    this.velx=0;
+    this.vely=0;
+    
+    this.moveSpeed=0.25;
+    this.maxSpeed=2.5;
+    
+    this.falling=false;
+    this.gravity=0.4;
+    this.color="red";
+};
 
-function update(){
-	ctx.clearRect(player.x-50, player.y-50, player.width+100, player.height+100);
-	
-	// check keys
-   if (keys[38] || keys[32]) {
-    // up arrow or space
-	  if(!player.jumping && player.grounded){
-	   player.jumping = true;
-	   player.grounded = false; // We're not on the ground anymore!!
-	   player.velY = -player.speed*player.jumpHeight;
-	  }
-	}
-   if (keys[39]) {
-       // right arrow
-       if (player.velX < player.speed) {                         
-           player.velX++;                  
-       }          
-   }          
-   if (keys[37]) {                 
-        // left arrow                  
-       if (player.velX > -player.speed) {
-           player.velX--;
-       }
-   }
-   
-   //enters friction
-   player.velX *= friction;
-   //adds gravity
-   player.velY += gravity;
-
-	//keeps player on screen
-	if (player.x >= width-player.width) {
-    player.x = width-player.width;
-	} else if (player.x <= 0) {
-    player.x = 0;
-	}
-	//so we can fall off ledges
-	player.grounded = false;
-	//adds fill for boxes
-	ctx.fillStyle = "black";
-	//create boxes
-	ctx.beginPath();
-	for (var i = 0; i < boxes.length; i++) {
-		var dir = colCheck(player, boxes[i]);
-		if (dir === "l" || dir === "r") {
-		  	player.velX = 0;
-		    player.jumping = false;
-		} else if (dir === "b") {
-		    player.grounded = true;
-		    player.jumping = false;
-		} else if (dir === "t") {
-			player.velY *= -1;
-		}
-      ctx.fillStyle="black";
-	    ctx.rect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
-	}
-	ctx.fill();
-	if(player.grounded){
-     player.velY = 0;
+Player.prototype.update=function(blocks){
+    // key inputs and there responses
+    if(keys[38] || keys[32]){
+      if(!this.jumping && !this.falling){
+        this.vely=-8;
+        this.falling=true;
+      }
+    }
+    if(keys[39]){
+        this.velx+=this.moveSpeed/2;
+        this.frame+=this.frameSpeed;
+        this.direction="right";
+    }
+    
+    if(keys[37]){
+      
+        this.velx-=this.moveSpeed/2;
+        this.frame+=this.frameSpeed;
+        this.direction="left";
+        
+    }
+    
+    // if the right input key and the left input key are not pressed, decrease the velocity until it comes to a complete stop
+    if(!keys[37]&&
+        !keys[39])
+    {
+        if(this.velx>0){
+            this.velx-=this.moveSpeed/2;
+        }
+        if(this.velx<0){
+            this.velx+=this.moveSpeed/2;
+        }
     }
 
-    //moves player
-    player.x += player.velX;
-	player.y += player.velY;
+    // limit the player's speed
+    if(this.velx>this.maxSpeed){
+        this.velx=this.maxSpeed;
+    }
+    if(this.velx<-this.maxSpeed){
+        this.velx=-this.maxSpeed;
+    }
+    if(this.vely>12){
+        this.vely=12;
+    }
     
-    // draw our player
-    ctx.fillStyle = "red";
-    ctx.fillRect(player.x, player.y, player.width, player.height);
-    // run through the loop again
-    requestAnimationFrame(update);
-}
+    // update the x and y positions
+    this.x+=this.velx;
+    this.applyCollision(blocks,this.velx,0);
+    
+    this.falling=true;
+    this.y+=this.vely;
+    this.applyCollision(blocks,0,this.vely);
+    this.vely+=this.gravity;
+};
 
-function colCheck(shapeA, shapeB) {
-    // get the vectors to check against
-    var vX = (shapeA.x + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2)),
-        vY = (shapeA.y + (shapeA.height / 2)) - (shapeB.y + (shapeB.height / 2)),
-        // add the half widths and half heights of the objects
-        hWidths = (shapeA.width / 2) + (shapeB.width / 2),
-        hHeights = (shapeA.height / 2) + (shapeB.height / 2),
-        colDir = null;
- 
-    // if the x and y vector are less than the half width or half height, they we must be inside the object, causing a collision
-   if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {         // figures out on which side we are colliding (top, bottom, left, or right)        
-            var oX = hWidths - Math.abs(vX),             
-            oY = hHeights - Math.abs(vY);         
-            if (oX >= oY) {
-            if (vY > 0) {
-                colDir = "t";
-                shapeA.y += oY;
-            } else {
-                colDir = "b";
-                shapeA.y -= oY;
+Player.prototype.applyCollision=function(obj,velx,vely){
+    for(var i=0; i<obj.length; i++){
+        if(collide(this,obj[i]))
+        {
+            if(vely>0){
+                this.vely=0;
+                this.falling=false;
+                this.y=obj[i].y-this.h;
             }
-        } else {
-            if (vX > 0) {
-                colDir = "l";
-                shapeA.x += oX;
-            } else {
-                colDir = "r";
-                shapeA.x -= oX;
+            if(vely<0){
+                this.vely=0;
+                this.falling=true;
+                this.y=obj[i].y+obj[i].h;
+            }
+            if(velx<0){
+                this.velx=0;
+                this.x=obj[i].x+obj[i].w;
+            }
+            if(velx>0){
+                this.velx=0;
+                this.x=obj[i].x-this.w;
             }
         }
-    return colDir;
-	}
+    }
+};
+
+Player.prototype.draw= function() {
+    // draw the player
+    ctx.fillStyle=this.color;
+    ctx.fill();
+    ctx.fillRect(this.x,this.y,this.w,this.h,10);
+};
+
+// store 'Player' objects into an array
+var players=[];
+players.add=function(x,y,w,h,keyInputs,color){
+    players.push(new Player(x,y,w,h,keyInputs,color));
+};
+players.apply=function(blocks){
+    for(var i=0; i<players.length; i++){
+        players[i].draw();
+        players[i].update(blocks);
+    }
+};
+
+// Block Object
+var Block=function(x,y,w,h,color){
+    this.x=x;
+    this.y=y;
+    this.w=w;
+    this.h=h;
+    this.color=color;
+};
+
+Block.prototype.draw= function() {
+    ctx.fillStyle=this.color;
+    ctx.fill();
+    ctx.fillRect(this.x,this.y,this.w,this.h);
+};
+
+var blocks=[];
+blocks.add=function(x,y,w,h,color){
+    blocks.push(new Block(x,y,w,h,color));
+};
+blocks.apply=function(players){
+    for(var i=0; i<blocks.length; i++){
+        blocks[i].draw();
+    }
+};
+
+function update(){
+	ctx.clearRect(0, 0, width, height);
+  players.apply(blocks);
+  blocks.apply(players);
+  ctx.fillStyle="black";
+    // run through the loop again
+    requestAnimationFrame(update);
 }
 
 function map() {
     // this is the code that reads the data:
     // for loop for y axis
-    for(var i=0; i<data.length; i++){
+    for(var col=0; col<data.length; col++){
     // for loop for x axis
-    for(var j=0; j<data[i].length; j++){
-        if(data[i][j]===0){
+    for(var row=0; row<data[col].length; row++){
+        if(data[col][row]===0){
             // leave blank
             ctx.fillStyle="white";
             ctx.fill();
-            ctx.fillRect(j*25,i*25,25,25);
+            ctx.fillRect(row*25,col*25,25,25);
         }
-        if(data[i][j]===1){
+        if(data[col][row]===1){
             // create block
-            ctx.fillStyle="black";
-            ctx.fill();
-            boxes.push({
-            x: j*25,
-            y: i*25,
-            width: 25,
-            height: 25
-            });
+            blocks.add(row*40,col*40,40,40,"black");
         }
-        if(data[i][j]===2){
+        if(data[col][row]===2){
             // create player
-            ctx.fillStyle="red";
-            ctx.fill();
+            players.add(row*40,col*40,40,40);
         }
         
     }
